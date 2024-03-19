33420b6f2d29338c9e684705c4e9785c9575187d
boryanayordanova <boryana.yourdanova@gmail.com>
2024-03-20 00:46:17 +0200

    done

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,14 +1,18 @@
 import classes from './Post.module.css';
+import { Link } from 'react-router-dom';
 
 export default function Post({
+    id,
     author, 
     body
 }){
 
     return (
         <li className={classes.post}>
-            <p className={classes.author}>{author}</p>
-            <p className={classes.text}>{body}</p>
+            <Link to={id}>
+                <p className={classes.author}>{author}</p>
+                <p className={classes.text}>{body}</p>
+            </Link>
         </li>
     )
 }
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 import Post from "./Post";
 import classes from "./PostsList.module.css";
-import NewPost from "./NewPost";
-import Modal from './Modal';
-export default function PostsList({ 
-    isPosting, 
-    onStopPosting, 
-}){
+export default function PostsList(){
 
-    const [posts, setPosts] = useState([]);
+    //infinite loop:
+    // fetch('http://localhost:8080/posts').then(response => response.json()).then(data => {
+    //     setPosts(data.posts);
+    // });
 
-    function addPostHandler(postData){
-        // setPosts([postData, ...posts]);
-        setPosts((existingPosts) => [postData, ...existingPosts]);
-    }
+    const posts = useLoaderData();
 
     return (
         <>
-        {isPosting && (
-            <Modal onClose={onStopPosting} >
-                <NewPost
-                    onCancel={onStopPosting}
-                    onAddPost={addPostHandler}
-                />
-            </Modal>
+
+        {posts.length > 0 && (
+            <ul className={classes.posts}>
+                {/* <Post author="Bory" body="Check out the full course!"/> */}
+                {posts.map((post) => (
+                    <Post key={post.id} id={post.id} author={post.author} body={post.body}/>
+                ))}
+                
+            </ul>
         )}
-        <ul className={classes.posts}>
-            <Post author="Bory" body="Check out the full course!"/>                
-            {/* {posts} */}
-        </ul>
+        {posts.length === 0 && (
+            <div style={{textAlign: 'center', color: 'white'}}>
+                <h1>There are no posts yet.</h1>
+                <p>Start adding some!</p>
+            </div>
+        )}
+
         </>
     )
 }
